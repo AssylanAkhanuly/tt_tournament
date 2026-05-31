1bde13af245d603e8e37a4a008761927520f0c63
AssylanAkhanuly <assylan.akhanuly@gmail.com>
2026-05-31 14:23:43 +0500

    fix: 30 days access token

--- back/tt_back/settings.py
+++ back/tt_back/settings.py
@@ -120,8 +120,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=30),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=30),
     "ROTATE_REFRESH_TOKENS": True,
     "AUTH_COOKIE": "access_token",
     "AUTH_COOKIE_REFRESH": "refresh_token",
